60860232b6880b88690cb001f5bf716d5a01ee1e
maruchan38 <162177476+maruchan38@users.noreply.github.com>
2024-03-05 10:38:47 -0500

    Add files via upload

---
+++ par o impar.py
@@ -0,0 +1,13 @@
+print("====================================================")
+print("programa para determinar si un numero es par o impar")
+print("==================================================== \n")
+
+numero = int(input("introduce el numero a analizar"))
+
+if numero %2 == 0:
+    print("el numero ", numero, "es par")
+
+else:
+    print("el numero es impar")
+
+    
