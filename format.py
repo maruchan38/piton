f3b741f57a187affe9ef4ff8d0c989f61648c09e
maruchan38 <162177476+maruchan38@users.noreply.github.com>
2024-03-09 10:28:57 -0500

    fromat concatenation
    this is  today's class file

---
+++ format.py
@@ -0,0 +1,9 @@
+name = "carlos"
+age = 22
+
+print("hello {} you are {} years old .\n".format(name ,age))
+
+
+#second form
+
+print("hello {name} you are {age} years old .".format(name = "carlos" ,age = 22))
