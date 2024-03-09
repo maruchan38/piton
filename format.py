764cb07e44f25c78e9a0f384cfb53f713ed13708
maruchan38 <162177476+maruchan38@users.noreply.github.com>
2024-03-09 10:34:08 -0500

    Update format.py

--- format.py
+++ format.py
@@ -7,3 +7,7 @@ print("hello {} you are {} years old .\n".format(name ,age))
 #second form
 
 print("hello {name} you are {age} years old .".format(name = "carlos" ,age = 22))
+
+#third form
+
+print("hello {0} you are {1} years old .".format(name ,age))
